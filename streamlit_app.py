167b364ea6bdd17722515de9bca30f658da32b23
Harimadhavmatta <102596520+Harimadhavmatta@users.noreply.github.com>
2024-08-12 13:52:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title(' 🎂 Bakery Customer Analysis')
 st.info(" Analyzer that shows what's happening in bakery ")
 st.write('Hello world!')
+df=pd.read_csv('bakery_customer_data.csv')
+st.write(df.head())
