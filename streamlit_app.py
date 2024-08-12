358de67fa120b09d6fc4897926d4d4b869f883bb
Harimadhavmatta <102596520+Harimadhavmatta@users.noreply.github.com>
2024-08-12 14:22:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,12 @@ df=pd.read_csv('bakery_customer_data.csv')
 st.subheader("Sample Dataset")
 st.write(df.head())
 st.subheader('Basic Description Of Data')
-with st.expander("**Info**"): 
-  st.write(" This info tells you about datasets schema ")
-  st.write(str(df.info()))
 with st.expander("**Describe**"):
   st.write(" This Describe display's the aggregates of each numeric column ")
   st.write(df.describe())
+
+st.subhead("want to know you top customers ? ")
+st.write("who many customers you want to see ? ")
+x=st.text_input('eg:- 10,20,5...')
+if x is not None:
+  st.wite(df.nlargest('bill_amount',x))
