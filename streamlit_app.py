925dee591a0150c83d8b573ee3ccd41dd74f9694
Harimadhavmatta <102596520+Harimadhavmatta@users.noreply.github.com>
2024-08-12 14:04:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
 st.title(' 🎂 Bakery Customer Analysis')
-st.info(" Analyzer that shows what's happening in bakery ")
-st.write('Hello world!')
+st.info(" Analyzer tells you what happened this year in the bakery ")
 df=pd.read_csv('bakery_customer_data.csv')
+st.subheader("Sample Dataset")
 st.write(df.head())
+st.subheader('Basic Description Of Data')
+with st.extender("**Info**"):
+  st.write(" This info tells you about datasets schema ")
+  st.write(df.info())
+with st.extender("**Describe**"):
+  st.write(" This Describe display's the aggregates of each numeric column ")
+  st.write(df.info())
