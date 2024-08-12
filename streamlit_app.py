e7a169915eb24bdf235fe663154838e3db6472e4
Harimadhavmatta <102596520+Harimadhavmatta@users.noreply.github.com>
2024-08-12 14:37:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,4 @@ with st.expander("**Top List**"):
   st.write("Who many top customers you want to see ? ")
   x=st.text_input('eg:- 10,20,5...')
   s=df.sort_values("bill_amount", ascending=False)
-  if x is not None:
-    st.write(s.head(x))
-  else:
-    str.write(" enter ")
+  
